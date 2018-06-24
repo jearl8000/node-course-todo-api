e3db511a59de34f9373c82102a7d1f30db6eda56
Jennifer Earl <jearl@braeburn.local>
2018-06-23 19:22:04 -0700

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,11 +20,21 @@ app.post('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
-})
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos: todos
+        });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
-})
+});
 
 module.exports = {
     app: app
