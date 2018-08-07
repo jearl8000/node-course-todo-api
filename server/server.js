37477e0c6b0cea8b544a87708d12aa9f6b89496f
Jennifer Earl <jearl@braeburn.local>
2018-08-07 08:14:51 -0700

    started converting routes to async/await

--- server/server.js
+++ server/server.js
@@ -63,21 +63,22 @@ app.get('/todos/:id', authenticate, (req, res) => {
     }
 });
 
-app.delete('/todos/:id', authenticate, (req, res) => {
+app.delete('/todos/:id', authenticate, async (req, res) => {
     var id = req.params.id;
     if (ObjectID.isValid(id)){ 
-        Todo.findOneAndRemove({
-            _id: id,
-            _creator: req.user._id
-        }).then((todo) => {
+        try {
+            const todo = await Todo.findOneAndRemove({
+                _id: id,
+                _creator: req.user._id
+            });
             if(todo) {
                 res.status(200).send({todo});
             } else {
                 res.status(404).send();
             }
-        }).catch((e)=> {
+        } catch (e) {
             res.status(400).send(e);
-        });
+        }
     } else {
         res.status(404).send();
     }
@@ -112,46 +113,42 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 
 });
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     // console.log(req.body);
-    var body = _.pick(req.body, ["email", "password"]);
-    var user = new User(body);
+    const body = _.pick(req.body, ["email", "password"]);
+    const user = new User(body);
 
-    user.save().then(() => {
-        // console.log("are there tokens??", user.tokens);
-        // res.status(200).send(user);
-        return user.generateAuthToken();
-    }).then((token) => {
-        // console.log("is there an auth token??", token);
+    try {
+        await user.save();
+        const token = await user.generateAuthToken();
         res.status(200).header('x-auth', token).send(user);
-    }).catch((err) => {
-        res.status(400).send(err);
-    });
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-app.post('/users/login', (req, res) => {
-    var body = _.pick(req.body, ["email", "password"]);
-    var user = new User(body);
-
-    User.findByCredentials(body.email, body.password).then((user) => {
-        return user.generateAuthToken().then((token) => {
-            res.status(200).header('x-auth', token).send(user);
-        });
-    }).catch((error) => {
+app.post('/users/login', async (req, res) => {
+    try {
+        const body = _.pick(req.body, ["email", "password"]);
+        const user = await User.findByCredentials(body.email, body.password);
+        const token = await user.generateAuthToken();
+        res.status(200).header('x-auth', token).send(user);
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-    req.user.removeToken(req.token).then(() => {
+app.delete('/users/me/token', authenticate, async (req, res) => {
+    try { 
+        await req.user.removeToken(req.token);
         res.status(200).send();
-    }, () =>{
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
 // pre-middlewareifying
